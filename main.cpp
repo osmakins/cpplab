8b118278addce2feb477d8bb79d4afbdb7a4f930
Oluwaseyi Akindayo <matthewakinse@gmail.com>
2019-10-15 20:01:25 +0200

    print sizes of variable types

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-using namespace std;
-
 int main() 
 {
-    cout << "Hello world, I am ready for C++";
+    //print the sizes of each varible types available in C++
+    std::cout<<"int size = "<<sizeof(int)<<"\n";
+    std::cout<<"short size = "<<sizeof(short)<<"\n";
+    std::cout<<"long size = "<<sizeof(long)<<"\n";
+    std::cout<<"char size = "<<sizeof(char)<<"\n";
+    std::cout<<"float size = "<<sizeof(float)<<"\n";
+    std::cout<<"double size = "<<sizeof(double)<<"\n";
+    std::cout<<"bool size = "<<sizeof(bool)<<std::endl;
     return 0;
 }
