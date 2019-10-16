8ae9f46747624ef2cbba225793680b02aebe2e34
Oluwaseyi Akindayo <matthewakinse@gmail.com>
2019-10-16 10:07:32 +0200

    format output using iomanip or escape characters

--- vartypes/main.cpp
+++ vartypes/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 int main() 
 {
@@ -31,6 +32,30 @@ int main()
     {
         std::cout<<"I'm not so sure January is the best month\n";
     }
+
+    // using the iomanip library and escape characters to format output
+    std::cout<<"\n\nThe text without any formating\n";
+    std::cout<<"Ints"<<"Floats"<<"Doubles"<< "\n";
+    std::cout<<"\nThe text with setw(15)\n";
+    std::cout<<"Ints"<<std::setw(15)<<"Floats"<<std::setw(15)<<"Doubles"<< "\n";
+    std::cout<<"\n\nThe text with tabs\n";
+    std::cout<<"Ints\t"<<"Floats\t"<<"Doubles"<< "\n\n";
+
+    //further output formating example
+    int a = 45;
+    float b = 45.323;
+    double c = 45.5468;
+    int aa = a + 9;
+    float bb = b + 9;
+    double cc = c + 9;
+    int aaa = aa + 9;
+    float bbb = bb + 9;
+    double ccc = cc + 9;
+    
+    std::cout<<"Ints"<<std::setw(10)<<"Floats"<<std::setw(10)<<"Doubles"<< std::setw(10)<<"\n";
+    std::cout<<a<<"\t"<<b<<"\t"<<c<<"\n";
+    std::cout<<aa<<"\t"<<bb<<"\t"<<cc<<"\n";
+    std::cout<<aaa<<"\t"<<bbb<<"\t"<<ccc<<"\n";
 
     return 0;
 }
