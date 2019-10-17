5f53a02b294e705624d2d073b9d9c33c00b6b39b
M.O Akindayo <matthewakinse@gmail.com>
2019-10-17 08:13:37 +0300

    file I/O Stream

---
+++ fileIO/main.cpp
@@ -0,0 +1,40 @@
+/*The goal of this quiz is to practice writing and reading files.
+**Read the contents of input.txt and then write to it. 
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  string line;
+  //create an output stream to write to the file
+  //append the new lines to the end of the file
+  ofstream myfileI("input.txt", ios::app);
+  if (myfileI.is_open())
+  {
+    myfileI << "\nI am adding a line.\n";
+    myfileI << "I am adding another line.\n";
+    myfileI.close();
+  }
+  else
+    cout << "Unable to open file for writing";
+
+  //create an input stream to write to the file
+  ifstream myfileO("input.txt");
+  if (myfileO.is_open())
+  {
+    while (getline(myfileO, line))
+    {
+      cout << line << '\n';
+    }
+    myfileO.close();
+  }
+
+  else
+    cout << "Unable to open file for reading";
+
+  return 0;
+}
