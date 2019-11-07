dae595a5500dd1eaddc27ca055ebf04f7a6fbd5f
M.O Akindayo <matthewakinse@gmail.com>
2019-11-07 10:22:54 +0200

    Repeat number guess using do while loop for stopping condition.

--- cflow/loops.cpp
+++ cflow/loops.cpp
@@ -8,26 +8,39 @@ int main()
   //use 55 as the number to be guessed
   int target = 55;
   int guess = -1;
+  int count = 0;
   std::cout << "Guess a number between 0 and 100: ";
   std::cin >> guess;
-  std::cout << guess << "\n";
 
-  while (guess != target)
+  do
   {
     if (guess < target)
     {
-      cout << guess << " is too low, try again!\n";
+      cout << guess << " is too low, try again!\n\n";
+      cout << "Enter a higher number: ";
       cin >> guess;
+      cout << "\n";
+      count++;
     }
-    if (guess > target)
+    else if (guess > target)
     {
-      cout << guess << " is too high, try again!\n";
+      cout << guess << " is too high, try again!\n\n";
+      cout << "Enter a lower number: ";
       cin >> guess;
+      cout << "\n";
+      count++;
     }
+    else
+    {
+      cout << "you have guessed the right number " << guess << "\n\n";
+      break;
+    }
+
+  } while (count < 5);
+
+  if (count >= 5)
+  {
+    std::cout << "Too many tries already. Come back later!\n\n";
   }
-
-  cout << "you have guessed the right number.\n"
-       << guess;
-
   return 0;
 }
