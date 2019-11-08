f3624e3fe7612929c5748ad8412e7eb9222705fa
M.O Akindayo <matthewakinse@gmail.com>
2019-11-08 12:42:03 +0200

    Make the guess a bit tougher with random number

--- cflow/loops.cpp
+++ cflow/loops.cpp
@@ -1,44 +1,58 @@
+/*Goal: In the programming quiz, use a while loop to prompt
+**the user to guess a target number. 
+**Tell the user if the guess is too high or too low. 
+**The user enters 'q'  or guesses the target number to end 
+**the program.
+*/
+
 #include <iostream>
 #include <sstream>
+#include <time.h>  //added for the random number generator seed
+#include <cstdlib> //added to use the rand function
 
 using namespace std;
 
 int main()
 {
-  //use 55 as the number to be guessed
-  int target = 55;
+  srand(time(NULL));             //set the seed for the random number generator
+  int target = rand() % 100 + 1; //generate the 'random' number
   int guess = -1;
+  std::string userString;
   int count = 0;
   std::cout << "Guess a number between 0 and 100: ";
-  std::cin >> guess;
 
   do
   {
+    std::getline(std::cin, userString);
+    //convert to an int
+    std::stringstream(userString) >> guess;
+    // std::cout << userString << "\n";
+    if (userString == "q")
+    {
+      std::cout << "Good bye! The number was " << target << "\n";
+      break;
+    }
     if (guess < target)
     {
       cout << guess << " is too low, try again!\n\n";
-      cout << "Enter a higher number: ";
-      cin >> guess;
-      cout << "\n";
+      cout << "Enter another number: ";
       count++;
     }
     else if (guess > target)
     {
       cout << guess << " is too high, try again!\n\n";
-      cout << "Enter a lower number: ";
-      cin >> guess;
-      cout << "\n";
+      cout << "Enter another number: ";
       count++;
     }
     else
     {
-      cout << "you have guessed the right number " << guess << "\n\n";
+      cout << "Congratulations! you have guessed the right number. \n\n The right number is: " << guess << "\n\n";
       break;
     }
 
-  } while (count < 5);
+  } while (count < 10);
 
-  if (count >= 5)
+  if (count >= 10)
   {
     std::cout << "Too many tries already. Come back later!\n\n";
   }
