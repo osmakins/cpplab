92f9d8b709388f50be870396535cde710c71b7a3
M.O Akindayo <matthewakinse@gmail.com>
2019-11-10 12:09:36 +0200

    Dereferencing pointer

--- pointers/main.cpp
+++ pointers/main.cpp
@@ -5,7 +5,8 @@
 int main()
 {
   int a = 54;
-  std::cout << "a = " << a << "\n";
-  std::cout << "address of a is at &a = " << &a << "\n";
+  int *pointerToA = &a;
+  std::cout << "pointerToA gets the address of a = " << pointerToA << "\n";
+  std::cout << "*PointerToA gets the value of a = " << *pointerToA << "\n";
   return 0;
 }
