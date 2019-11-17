5b43c4c6d535a526cc8b23f1ec4574524de31c75
M.O Akindayo <matthewakinse@gmail.com>
2019-11-17 21:48:04 +0200

    Searching through an array

--- functions/afunc.cpp
+++ functions/afunc.cpp
@@ -1,48 +1,30 @@
-/*Goal: Learn to pass arrays to functions*/
+/*Goal: create a function that searches an array for 
+**a given value. 
+*/
 
 #include <iostream>
-#include <iomanip>
 
-//Pass the array as a pointer
-void arrayAsPointer(int *array, int size);
-//Pass the array as a sized array
-void arraySized(int array[3], int size);
-//Pass the array as an unsized array
-void arrayUnSized(int array[], int size);
+using namespace std;
+
+void search(int array[3], int size, int searchKey);
 
 int main()
 {
-  const int size = 3;
-  int array[size] = {33, 66, 99};
-  //We are passing a pointer or reference to the array
-  //so we will not know the size of the array
-  //We have to pass the size to the function as well
-  arrayAsPointer(array, size);
-  arraySized(array, size);
-  arrayUnSized(array, size);
+  const int size = 4;
+  int array[] = {345, 75896, 2, 543};
+  int searchKey = 543;
+  cout << "Found at: ";
+  search(array, size, searchKey);
   return 0;
 }
 
-void arrayAsPointer(int *array, int size)
+void search(int array[4], int size, int searchKey)
 {
-  std::cout << std::setw(5);
   for (int i = 0; i < size; i++)
-    std::cout << array[i] << " ";
-  std::cout << "\n";
-}
-
-void arraySized(int array[3], int size)
-{
-  std::cout << std::setw(5);
-  for (int i = 0; i < size; i++)
-    std::cout << array[i] << " ";
-  std::cout << "\n";
-}
-
-void arrayUnSized(int array[], int size)
-{
-  std::cout << std::setw(5);
-  for (int i = 0; i < size; i++)
-    std::cout << array[i] << " ";
-  std::cout << "\n";
+  {
+    if (searchKey == array[i])
+    {
+      cout << i << " ";
+    }
+  }
 }
