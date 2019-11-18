d1a149bd5491adc95963fdf83e3cd18c57c0744d
M.O Akindayo <matthewakinse@gmail.com>
2019-11-18 12:49:51 +0200

    Clean up with the use of header files

--- classes/main.cpp
+++ classes/main.cpp
@@ -1,58 +1,5 @@
 /*Goal: Practice using a class*/
-
-#include <iostream>
-using namespace std;
-
-class Student
-{
-  string name;
-  int id;
-  int gradDate;
-
-public:
-  void setName(string nameIn);
-  void setId(int idIn);
-  void setGradDate(int dateIn);
-  string getName();
-  int getId();
-  int getGradDate();
-  void print();
-};
-
-void Student::setName(string nameIn)
-{
-  name = nameIn;
-}
-
-void Student::setId(int idIn)
-{
-  id = idIn;
-}
-
-void Student::setGradDate(int gradDateIn)
-{
-  gradDate = gradDateIn;
-}
-
-void Student::print()
-{
-  cout << name << " " << id << " " << gradDate;
-}
-
-string Student::getName()
-{
-  return name;
-}
-
-int Student::getId()
-{
-  return id;
-}
-
-int Student::getGradDate()
-{
-  return gradDate;
-}
+#include "main.hpp"
 
 int main()
 {
